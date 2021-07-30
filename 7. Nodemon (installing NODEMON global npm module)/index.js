695e1bcbe72974c3e34229d22f3c9815a05efa32
Rushabh Halmare <42460595+rishabhhalmare07@users.noreply.github.com>
2021-07-30 19:05:06 +0530

    nodemon

---
+++ 7. Nodemon (installing NODEMON global npm module)/index.js
@@ -0,0 +1,10 @@
+//Nodemon is a global npm module
+//It is a tool that helps to develop node.js application automatically it restarts if any changes is made
+
+//Terminal
+//npm install nodemon -g = INstall NODEMON by this command
+//nodemon index.js = To run any file via nodemon
+//CTRL + C  = To close Nodemon 
+
+
+console.log("Testing and Running Nodemon");
